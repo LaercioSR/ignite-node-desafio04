1d55e5d5896cb6ad354d6942d6e1af31ce4100b9
Laercio de Souza Rios <laercio.rios13@hotmail.com>
2021-11-19 16:20:49 -0300

    :adhesive_bandage: Fixed error in user not admin get list

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -11,7 +11,7 @@ class ListAllUsersUseCase {
   execute({ user_id }: IRequest): User[] | undefined {
     const user = this.usersRepository.findById(user_id);
 
-    if (user) {
+    if (!user.admin) {
       return undefined;
     }
 
